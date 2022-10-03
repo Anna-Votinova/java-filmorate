f88fde39c40e56aafaff1b5de5ae4b3efa1d3dc5
Anna Votinova <anyvotinova@yandex.ru>
2022-10-03 19:02:10 +0300

    fix: проведен небольшой рефакторинг

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class MpaRating {
+    private int id;
+    @NotBlank
+    private String name;
+
+}
